ce144f1fa098142b7767b679c76f1f77cecbd8e1
notRenaissance <tylercox502@gmail.com>
2017-09-14 05:03:52 -0400

    Add files via upload

--- object_stack.gd
+++ object_stack.gd
@@ -3,7 +3,6 @@ extends Control
 var item  = null
 var count = 0
 var sprite
-var label
 
 const SPRITE_SIZE = 32  # the size of the sprite
 const ICON_SIZE = 64
@@ -14,8 +13,6 @@ func _init(i, c):
 	count = c
 	sprite = Sprite.new()
 	add_child(sprite)
-	label = Label.new()
-	add_child(label)
 	set_size(Vector2(ICON_SIZE, ICON_SIZE))
 
 func _ready():
@@ -30,16 +27,6 @@ func layout():
 	sprite.set_texture(preload("res://items.png"))
 	sprite.set_region(true)
 	sprite.set_region_rect(Rect2(ICON_SIZE * (icon % RAW_LENGTH), ICON_SIZE * (icon / RAW_LENGTH), ICON_SIZE, ICON_SIZE))
-	if count > 1:
-		label.show()
-		label.set_align(Label.ALIGN_RIGHT)
-		label.add_color_override("font_color", Color(0, 0, 0))
-		label.add_color_override("font_color_shadow", Color(50, 50, 50))
-		label.set_text(str(count))
-		label.set_pos(Vector2(get_size().x-40, 0))
-		label.set_size(Vector2(40, 10))
-	else:
-		label.hide()
 
 func can_stack(s):
 	return s.item == item
